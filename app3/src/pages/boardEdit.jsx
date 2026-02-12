f8371f524c60afca649fe46cf36b560954035d8f
SooAh0208 <santacoyaki11@gmail.com>
2026-02-12 12:46:54 +0900

    boardEdit

--- app3/src/pages/boardEdit.jsx
+++ app3/src/pages/boardEdit.jsx
@@ -1,9 +1,31 @@
 const BoardEdit = () => {
   return (
-    <div className="text-center">
-      <h1>404</h1>
-      <p>페이지를 찾을 수 없습니다.</p>
-    </div>
+    <div className="container mt-3">
+		<h1 className="display-1 text-center">게시글 수정</h1>
+		<form>
+			<div className="mb-3 mt-3">
+				<label htmlFor="title" className="form-label">제목</label>
+				<input type="text" className="form-control" id="title" placeholder="제목을 입력하세요." name="title"/>
+			</div>
+			<div className="mb-3 mt-3">
+				<label htmlFor="name" className="form-label">작성자</label>
+				<input type="text" className="form-control" id="name" name="name" disabled/>
+			</div>
+			<div className="mb-3 mt-3">
+				<label htmlFor="content" className="form-label">내용</label>
+				<textarea type="text" className="form-control h-50" rows="10" placeholder="내용을 입력하세요."
+					name="content"></textarea>
+			</div>
+			<div className="d-flex">
+				<div className="p-2 flex-fill d-grid">
+					<button className="btn btn-primary">저장</button>
+				</div>
+				<div className="p-2 flex-fill d-grid">
+					<button className="btn btn-primary">취소</button>
+				</div>
+			</div>
+		</form>
+	</div>
   )
 }
 
